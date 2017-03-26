d221a1a4d8ee889289002a69e916bc90eca0f747
Rohana Rezel <rohana.rezel@riolet.com>
2017-03-25 17:38:28 -0700

    First commit

---
+++ pol.c
@@ -0,0 +1,47 @@
+/*
+* Author: Rohana Rezel
+* Acknowledgements:
+* Code concatenation
+* http://stackoverflow.com/questions/3126882/concatenate-all-arguments-except-the-executable-name
+*/
+#include <Python.h>
+
+int
+main(int argc, char *argv[])
+{
+    unsigned int i;
+
+    char *args, *python_source;
+
+    char * header = "from __future__ import print_function\n"
+             "import sys\n"
+             "_=[line.strip().split() for line in sys.stdin.readlines()]\n";
+
+    size_t len = strlen(header);
+
+    for(i=1; i<argc; i++) {
+        len += strlen(argv[i]);
+    }
+
+    args = python_source = (char *)malloc(len+argc-1);
+
+    memcpy(args, header, strlen(header));
+    args += strlen(header);
+
+    for(i=1; i<argc; i++) {
+        memcpy(args, argv[i], strlen(argv[i]));
+        args += strlen(argv[i])+1;
+        *(args-1) = ' ';
+    }
+
+    *(args-1) = 0;
+
+    Py_SetProgramName(argv[0]);  /* optional but recommended */
+    Py_Initialize();
+
+    PyRun_SimpleString(python_source);
+
+    Py_Finalize();
+    free(python_source);
+    return 0;
+}
