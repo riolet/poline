be77ed9ff10bd2aa39e99fc8fa5ff7bb550bab1a
Rohana Rezel <rohana.rezel@riolet.com>
2017-03-25 22:50:08 -0700

    Placeholder for command line options

--- pol.c
+++ pol.c
@@ -8,12 +8,19 @@
 * Argument concatenation
 * http://stackoverflow.com/questions/3126882/concatenate-all-arguments-except-the-executable-name
 */
+#include <unistd.h>
+#include <stdio.h>
 #include <Python.h>
 
-int
-main(int argc, char *argv[])
+void usage(void)
+{
+    printf ("pol [options] string\n");
+}
+
+int main(int argc, char *argv[])
 {
     unsigned int i;
+    char opt;
 
     char *args, *python_source;
 
@@ -22,9 +29,26 @@ main(int argc, char *argv[])
              "sortedbycount = lambda l, reverse=False: sorted([{'e':e, 'c':l.count(e)} for e in sorted(set(l))], key=lambda k: k['c'], reverse=reverse)\n"
              "_=[line.strip().split() for line in sys.stdin.readlines()]\n";
 
+    while ((opt = getopt (argc, argv, "h")) != -1) {
+        switch (opt) {
+          case 'h':
+            usage();
+            exit(0);
+            break;
+          default:
+            usage();
+            exit(1);
+         }
+     }
+
     size_t len = strlen(header);
 
-    for(i=1; i<argc; i++) {
+    if (argv[optind] == NULL || argv[optind + 1] == NULL) {
+      usage();
+      exit(1);
+    }
+
+    for(i=optind; i<argc; i++) {
         len += strlen(argv[i]);
     }
 
@@ -33,7 +57,7 @@ main(int argc, char *argv[])
     memcpy(args, header, strlen(header));
     args += strlen(header);
 
-    for(i=1; i<argc; i++) {
+    for(i=optind; i<argc; i++) {
         memcpy(args, argv[i], strlen(argv[i]));
         args += strlen(argv[i])+1;
         *(args-1) = ' ';
