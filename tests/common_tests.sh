b2cb288f9c89dfccb1777e4715bb6e6c787e956f
Rohana Rezel <rohana.rezel@riolet.com>
2017-03-29 06:31:08 -0700

    Adding a set of common tests

--- tests/common_tests.sh
+++ tests/common_tests.sh
@@ -7,4 +7,4 @@ echo "Test: '{}\t{}'.format(x,c) for x, c in counter(l[1] for l in _ if l[1])"
 ls -lah | PYTHONPATH=`pwd` $1 poline/poline.py -s "'{}\t{}'.format(x,c) for x, c in counter(l[1] for l in _ if l[1])"
 
 echo "Test: i[5] for i in netstat('-tulpn',s=True)"
-PYTHONPATH=`pwd` $1 poline/poline.py "i[5] for i in netstat('-tulpn',s=True)"
+PYTHONPATH=`pwd` $1 poline/poline.py "i[5] for i in ls('-lah',s=True)"
