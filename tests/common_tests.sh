fb5c6bb4c57038fccc576ae0433007adcae83ff9
Rohana Rezel <rohana.rezel@riolet.com>
2017-03-30 18:39:48 -0700

    Set unicode encoding

--- tests/common_tests.sh
+++ tests/common_tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH=`pwd`
+export PYTHONIOENCODING=UTF-8
+
 export testpol="${1} poline/poline.py"
 
 echo 'Test: repr(x) for x in _'
@@ -10,7 +12,7 @@ echo "Test: '{}\t{}'.format(x,c) for x, c in counter(l[1] for l in _ if l[1])"
 ls -lah | ${testpol} -s "'{}\t{}'.format(x,c) for x, c in counter(l[1] for l in _ if l[1])"
 
 echo "Test: i[5] for i in netstat('-tulpn',s=True)"
-testpol "i[5] for i in ls(['-lah'],s=True)"
+${testpol} "i[5] for i in ls(['-lah'],s=True)"
 
 echo "Test: df as bar graph"
 df -B1 | ${testpol} -s "'{:10.10}\t{:10.10}\t{:10.10}\t{:10.10}\t{:5.5}\t{}{}\t{:10.10}'.format(i[0],bytesize(i[1],f=True),bytesize(i[2],f=True),bytesize(i[3],f=True),i[4],'#'*int(10*int(i[2])/int(i[1])+0.5) if i[1].isdigit() else ' '*5, '_'*(10-int(10*int(i[2])/int(i[1])+0.5)) if i[1].isdigit() else ' '*5,i[5]) for i in _"
